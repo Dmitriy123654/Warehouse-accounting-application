628e8ca467746ac0d0923d4a740e028e748794e5
Dmitriy Ermak <jamayka.10@mail.ru>
2022-11-14 22:04:52 +0300

    добавлены основные модели данных

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace test4
     {
         public static void Main(string[] args)
         {
-            system("chcp 1251");
+
             //using (ApplicationContext db = new ApplicationContext())
             //{
             //    User user1 = new User { Name = "Tom", Age = 33 };
@@ -35,17 +35,31 @@ namespace test4
     //    public string? Name { get; set; }
     //    public int Age { get; set; }
     //}
-    public enum Category
+    //public enum Category
+    //{
+    //    Mobiles,
+    //    TV,
+    //    Laptops
+    //}
+    //public enum Address
+    //{
+    //    Adres1,
+    //    Adres2,
+    //    Adres3
+    //}
+    public class Address
     {
-        Mobiles,
-        TV,
-        Laptops
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //связи
+
     }
-    public enum Address
+    public class Category
     {
-        Adres1,
-        Adres2,
-        Adres3
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //связи
+        public List<Product> Products { get; set; } = new();
     }
     /// <summary>
     /// Товар
@@ -53,8 +67,13 @@ namespace test4
     public class Product
     {
         public int Id { get; set; }
+        public int Number { get; set; }
         public string? Name { get; set; }
-        public Category Category { get; set; }
+        //связи:
+        //категория
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        //склад
         public int LocationWarehouseNumber { get; set; }// склад товара
 
     }
@@ -67,26 +86,61 @@ namespace test4
         public Address Address { get; set; }
         public int RackNumber { get; set; }//стеллаж
         public int ShelfNumber { get; set; }//полка
+        //связи:
 
+        //товары
         public List<Product> Products { get; set; } = new();//товары склада
     }
-    public interface Person
+    //public interface Person
+    //{
+    //    public string FirstName { get; set; }
+    //    public string SecondName { get; set; }
+    //    public string LastName { get; set; }
+    //    public int Age  { get; set; }
+    //}
+    public class User 
     {
+        public int Id { get; set; }
+        public int Number { get; set; }
         public string FirstName { get; set; }
-        public string SecondName { get; set; }
-        public string LastName { get; set; }
-        public int Age  { get; set; }
-    }
-    public class User : Person
-    {
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
-        public string LastName { get; set; }
+        public string? SecondName { get; set; }
+        public string? LastName { get; set; }
         public int Age { get; set; }
-        //POSITION..
+        
     }
-    //POSITION
+    public class Employee : User
+    {
+        public decimal Salary { get; set; }
+        //???добавить отпуск
+        //связи:
+        //отдел 
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+        // должность
+        public int PostId { get; set; }
+        public Post? Post { get; set; }
 
+    }
+    /// <summary>
+    /// отдел работы
+    /// </summary>
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //связи
+        public List<Employee> Employees { get; set; } = new();
+    }
+    /// <summary>
+    /// должность
+    /// </summary>
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //связь
+        public List<Employee> Employees { get; set; } = new();
+    }
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; } = null!;
@@ -105,8 +159,8 @@ namespace test4
         //допилить
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().HasCheckConstraint("Age", "Age > 0 AND Age < 120"); //проверка перед созданием
-            modelBuilder.Entity<User>().HasIndex(u => u.Age).IsUnique();//уникальность
+            //modelBuilder.Entity<User>().HasCheckConstraint("Age", "Age > 0 AND Age < 120"); //проверка перед созданием
+            //modelBuilder.Entity<User>().HasIndex(u => u.Age).IsUnique();//уникальность
         }
        
     }
