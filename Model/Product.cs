6627d0acb4bb261edbc30251c050d6808ceeeaa8
Dmitriy Ermak <jamayka.10@mail.ru>
2022-12-21 23:03:13 +0300

    Предкурсовая версия программы

---
+++ Model/Product.cs
@@ -0,0 +1,41 @@
+﻿namespace WarehouseInformationSystem.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Не указано имя товара")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Недопустимая длина имени")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Не указаны характеристики/описание")]
+        [StringLength(300, MinimumLength = 2, ErrorMessage = "Недопустимая длина имени")]
+        public string Characteristic { get; set; } 
+        [Required(ErrorMessage = "Не указана цена")]
+        public decimal SalePrice { get; set; }// цена продажи
+        public decimal? PurchasePrice { get; set; }//закупочная
+        public Product(string name, string characteristic, decimal salePrice, decimal? purchasePrice)
+        {
+            Name = name;
+            Characteristic = characteristic;
+            SalePrice = salePrice;
+            PurchasePrice = purchasePrice;
+           
+        }
+
+        //связи:
+        //категория
+        public int CategoryOfProductId { get; set; }
+        public CategoryOfProduct? CategoryOfProduct { get; set; }
+        //склад
+        public int LocationWarehouseNumber { get; set; }
+        public Location? Location { get; set; }
+        public override string ToString()
+        {
+            return $"Название: {Name}\n" +
+                $"Характеристики: {Characteristic}" +
+                $"Цена продажи: {SalePrice}\n" +
+                $"Цена закупки: {PurchasePrice}\n";
+        }
+
+       
+    }
+}
